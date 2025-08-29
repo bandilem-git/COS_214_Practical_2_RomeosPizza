24e172bab39e3d929be77889201b39f94d8ff784
bandilem-git <u24675394@tuks.co.za>
2025-08-29 14:57:30 +0200

    Decorator:  BasePizza class initialized take note of the following change:
    1: instead of toppings PizzaComponent please make it toppings : vector<PizzaComponent*> toppings

--- BasePizza.h
+++ BasePizza.h
@@ -1,5 +1,15 @@
 #ifndef BASEPIZZA_H
 #define BASEPIZZA_H
 
+#include "Pizza.h"
+#include "PizzaComponent.h"
+#include <vector>
+class BasePizza : public Pizza{
+    private:
+        vector<PizzaComponent*> toppings;
+    public:
+        double getPrice() override;
+        std::string getName() override;
+};
 
 #endif
