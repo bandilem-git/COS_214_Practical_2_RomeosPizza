071d4442e4614bc965a39e970ba85ae7ed773400
bandilem-git <u24675394@tuks.co.za>
2025-08-29 14:50:37 +0200

    Decorator: Pizza class implemented - take note of the following changes: 1- added name and price attributes 2 - added parameterized constructor

--- Pizza.h
+++ Pizza.h
@@ -1,5 +1,18 @@
 #ifndef PIZZA_H
 #define PIZZA_H
 
+#include <iostream>
+#include <string>
+using namespace std;
+class Pizza{
+    private:
+    std::string name;
+    double price;
+    public:
+        Pizza(std::string name, double price);
+        virtual double getPrice() = 0;
+        virtual std::string getName() = 0;
+        virtual void printPizza();
+};
 
 #endif
