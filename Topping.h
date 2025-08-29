c32575e079ed5f44b2a568b8b70cdf18765e2208
bandilem-git <u24675394@tuks.co.za>
2025-08-29 14:24:36 +0200

    Composite:  Topping  class constructor defined

--- Topping.h
+++ Topping.h
@@ -1,5 +1,11 @@
 #ifndef TOPPING_H
 #define TOPPING_H
+#include "PizzaComponent.h"
+
+class Topping: public PizzaComponent{
+    public:
+        Topping(std::string name, double price);
+};
 
 
 #endif
