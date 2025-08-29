f7344b45f24ba76f76a1bee7e63df5c5a141f81c
bandilem-git <u24675394@tuks.co.za>
2025-08-29 15:10:16 +0200

    Decorator: StuffedCheese and ExtraCheese classes initialised

--- ExtraCheese.h
+++ ExtraCheese.h
@@ -1,5 +1,11 @@
 #ifndef EXTRACHEESE_H
 #define EXTRACHEESE_H
 
+#include "PizzaDecorator.h"
 
+class ExtraCheese: public PizzaDecorator{
+    public:
+    double getPrice() override;
+    std::string getName() override;
+};
 #endif
--- StuffedCrust.h
+++ StuffedCrust.h
@@ -1,5 +1,13 @@
 #ifndef STUFFEDCRUST_H
 #define STUFFEDCRUST_H
 
+#include "PizzaDecorator.h"
+
+class StuffedCheese: public PizzaDecorator{
+    public:
+    double getPrice() override;
+    std::string getName() override;
+};
+
 
 #endif
