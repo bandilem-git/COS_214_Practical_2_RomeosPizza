1038b9c135ee752c02b67df92340970a710dbd83
bandilem-git <u24675394@tuks.co.za>
2025-08-29 14:18:20 +0200

    Composite: PizzaComponent class defined

--- PizzaComponent.h
+++ PizzaComponent.h
@@ -1,5 +1,18 @@
 #ifndef PIZZACOMPONENT_H
 #define PIZZACOMPONENT_H
 
+#include <iostream>
+#include <string>
+using namespace std;// will be used in testing
+
+class PizzaComponent{
+    private:
+        double price; // price of each topping i guess??
+        std::string name; // tomatoes, pepperoni ,chicken, salami, ham, pineapple 
+    public:
+        virtual std::string getName();
+        virtual double getPrice();
+        PizzaComponent(std::string name, double price);
+};
 
 #endif
