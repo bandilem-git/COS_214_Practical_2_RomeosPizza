60a8867c3d6468bb8d70ae4d5aecddcc73572de8
bandilem-git <u24675394@tuks.co.za>
2025-08-29 15:05:41 +0200

    Decorator: PizzaDecorator class initialised take note of the following changes:
    1: instead of Pizza pizza make it Pizza* pizza

--- PizzaDecorator.h
+++ PizzaDecorator.h
@@ -1,5 +1,14 @@
 #ifndef PIZZADECORATOR_H
 #define PIZZADECORATOR_H
 
+#include "Pizza.h"
+class PizzaDecorator: public Pizza{
+    private:
+        Pizza* pizza;
+    public:
+        virtual double getPrice() = 0;
+        virtual std::string getName()= 0;
+    };
+
 
 #endif
