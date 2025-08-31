61b020dc5c2d334965656ace344e146bc37e0f09
bandilem-git <u24675394@tuks.co.za>
2025-08-31 11:55:09 +0200

    Composite: getters in Topping.cpp were made

--- Topping.cpp
+++ Topping.cpp
@@ -1,3 +1,14 @@
 #include "Topping.h"
 
-Topping::Topping(std::string name, double price):PizzaComponent(name,price){}
+Topping::Topping(std::string name, double price):PizzaComponent(name,price){
+    this->name = name;
+    this->price = price;
+}
+
+std::string Topping::getName(){
+    return name;
+}
+
+double Topping::getPrice(){
+    return price;
+}
