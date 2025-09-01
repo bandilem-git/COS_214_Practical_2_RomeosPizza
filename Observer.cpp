b8b7b292e5d416a1a5d21c3a1858f6c20369b2af
WiseKRB <krbnkomo24@gmail.com>
2025-09-01 07:25:48 +0200

    observer that should work as intended (I think)

--- Observer.cpp
+++ Observer.cpp
@@ -2,10 +2,10 @@
 #include "Menus.h"
 
 void Observer::setSubject(Menus* s){
-    if (subject && subject != s)
-    { // deregistering from old subject
-        subject->removeObserver(this);
-    }
+    // if (subject && subject != s)
+    // { // deregistering from old subject
+    //     subject->removeObserver(this);
+    // }
 
     subject = s;
 } // to be used by subject when we addObserver
