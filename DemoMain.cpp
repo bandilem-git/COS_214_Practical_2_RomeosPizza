f73c7a723a0f7f79d6c82c5b611e3d70dcaff34c
bandilem-git <u24675394@tuks.co.za>
2025-09-01 13:36:56 +0200

    created DemoMain and included headers

---
+++ DemoMain.cpp
@@ -0,0 +1,26 @@
+// For Demo purposes:
+
+//Composite
+#include "Topping.h"
+#include "ToppingGroup.h"
+
+//Decorator
+#include "BasePizza.h"
+#include "StuffedCrust.h"
+#include "ExtraCheese.h"
+#include "Decker.h"
+
+//strategy
+#include "RegularPrice.h"
+#include "BulkDiscount.h"
+#include "FamilyDiscount.h"
+
+//observer
+#include "PizzaMenu.h"
+#include "SpecialsMenu.h"
+#include "Customer.h"
+#include "Website.h"
+
+int main(){
+    
+}
